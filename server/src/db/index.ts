676be06dff7f92d94be76e9535c9aa3f86691af5
Paing Thu Aung <paing.pta@gmail.com>
2026-01-08 19:06:15 +0630

    refactor db config

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     let DB_CONNECTION: string;
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV === "production") {
+      if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
+      DB_CONNECTION = process.env.MONGO_URI;
+    } else {
       if (!process.env.MONGO_LOCAL_URI)
         throw new Error("MONGO_LOCAL_URI is not defined");
       DB_CONNECTION = process.env.MONGO_LOCAL_URI;
-    } else {
-      if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
-      DB_CONNECTION = process.env.MONGO_URI;
     }
     const dbResponse = await mongoose.connect(DB_CONNECTION);
     console.log("Connected to MongoDB", dbResponse.connection.host);
